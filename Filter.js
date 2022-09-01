9725fb88c996b83ce14fe428eaa6bc9833f4cbaa
Mahesh <itstribe.mahi@gmail.com>
2022-09-01 23:28:50 +0530

    Filter method

---
+++ Filter.js
@@ -0,0 +1,11 @@
+// // filter  
+// let names=['Dwayne Johnson','Roman Reighns',"gal gadot",'NTR','Mahesh', "Ronaldo"];
+// let check=names.filter(word=>word.length<7);
+// console.log(check); 
+
+// let num=[12345,23456,23456,1435,3245,2345678,213245];
+// let resma=num.filter(x=>x%5==0);
+// console.log(resma);
+
+
+
