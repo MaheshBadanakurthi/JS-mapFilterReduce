940d7ac6351ad3423383517c4fd45689d21198d3
Mahesh <itstribe.mahi@gmail.com>
2022-09-01 23:30:42 +0530

    Prime numbers

---
+++ prime.js
@@ -0,0 +1,12 @@
+const array = [53,97, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+console.log(array.filter(isPrime));
