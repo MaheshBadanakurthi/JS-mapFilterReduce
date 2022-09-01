29c1a7afbce90ccf2dae3106418b76d9e3a54804
Mahesh <itstribe.mahi@gmail.com>
2022-09-01 23:31:29 +0530

    Map method

---
+++ mapFunction.js
@@ -0,0 +1,28 @@
+// map for multiplying numbers
+// let numbers=[123,456,3451,23,123]
+// let newNumbers=numbers.map(mapFunction);
+//  function mapFunction(num){
+//     return num*5;
+//  }
+//  console.log(newNumbers);
+
+
+// //  for multiply
+// let num=[23,3456,2345123,23]
+// let result=num.map(x=>x*3);
+// console.log(result);
+
+// //  cube  root;
+// let  str=[121,125,343,789];
+// let cube=str.map(x=>Math.cbrt(x));
+// console.log(cube);
+
+
+//objects
+let samObject=[{key:'movie', value:'RRR'},     {key:'Music',value:'Keeravani'},    {key:'Heros' ,value:'NTR RamCharan'}]
+let result=samObject.map(({key,value})=>({[key]:value}));
+console.log(result)
+
+
+
+
