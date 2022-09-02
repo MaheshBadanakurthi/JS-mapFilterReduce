0af08921875cae1a6f8bfa214819428b4e65372c
Mahesh <itstribe.mahi@gmail.com>
2022-09-02 11:09:51 +0530

    reduce

---
+++ reduce.js
@@ -0,0 +1,13 @@
+// for adding using reduce
+// let num=[234,65,345,345,11]
+// let result=num.reduce((x,y)=>x-y);
+// console.log(result);
+
+// savings from cuttings.
+// let cuttings=[5000,1000,200,3000]
+// let salary=20000;
+// let sal=1000;
+// let result=cuttings.reduce((x,y)=>x-y,salary,sal);// here only salary will take rest will not take.
+// console.log(result);
+
+
